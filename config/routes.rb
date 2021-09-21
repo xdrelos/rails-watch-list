9ba3ee8037e16746fe7df68fd8b0eee7daa7171f
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-21 19:33:18 +0200

    restrict devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [:sessions, :registrations]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
